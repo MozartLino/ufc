e2fb3f1da36783b906d033653d1e6e9df80c003b
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-06-03 15:03:49 -0300

    create class UsuarioRepresentation

---
+++ src/main/java/br/com/each/model/usuario/UsuarioRepresentation.java
@@ -0,0 +1,78 @@
+package br.com.each.model.usuario;
+
+public class UsuarioRepresentation {
+
+	private Long id;
+	private String cpf;
+	private String nome;
+	private String login;
+	private String senha;
+	private String email;
+	private Long pontos;
+	private int perfil;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(int perfil) {
+		this.perfil = perfil;
+	}
+
+	public Long getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(Long pontos) {
+		this.pontos = pontos;
+	}
+
+}
