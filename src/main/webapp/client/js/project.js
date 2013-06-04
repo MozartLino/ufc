c444ab8c0ed67cf8890fd7ab934908b28f9ed125
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-06-04 12:40:54 -0300

    refatoração

--- src/main/webapp/client/js/project.js
+++ src/main/webapp/client/js/project.js
@@ -1,16 +1,19 @@
 angular.module('project', []).config(function($routeProvider) {
-	$routeProvider.when('/usuarios/novo', {
+	$routeProvider.when('/usuario/novo', {
 		controller : usuarioForm,
 		templateUrl : 'client/html/usuario/form.html'
-	}).when('/usuarios/edita/:usuarioId', {
+	}).when('/usuario/edita/:usuarioId', {
 		controller : usuarioForm,
 		templateUrl : 'client/html/usuario/form.html'
 	}).when('/perfil', {
 		controller : usuarioForm,
-		templateUrl : 'client/html/usuario/ranking.html'
+		templateUrl : 'client/html/usuario/perfil.html'
 	}).when('/ranking', {
 		controller : usuarioForm,
-		templateUrl : 'client/html/usuario/perfil.html'
+		templateUrl : 'client/html/usuario/ranking.html'
+	}).when('/login', {
+		controller : usuarioForm,
+		templateUrl : 'client/html/usuario/login.html'
 	}).otherwise({
 		redirectTo : '/'
 	});
