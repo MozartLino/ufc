375e14ebad9c67321c7c6d54f7a81e9e9a701700
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-06-04 15:27:07 -0300

    alterando a conexão com o banco

--- src/main/java/br/com/each/ConnectionFactory/ConnectionFactory.java
+++ src/main/java/br/com/each/ConnectionFactory/ConnectionFactory.java
@@ -4,19 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {  
-    
-	private static String DRIVER = "com.mysql.jdbc.Driver";  
-    private static String URL = "jdbc:mysql://localhost/makeyourbad";  
-    private static String USER = "root";  
-    private static String PASSWD = "xxx";  
-      
-    public static Connection getConnection() throws SQLException{  
-        try {  
-            Class.forName(DRIVER);  
-            return DriverManager.getConnection(URL,USER,PASSWD);  
-        } catch (ClassNotFoundException e){  
-            throw new SQLException(e.getMessage());   
-        }        
-    }     
+public class ConnectionFactory {
+
+	private static String DRIVER = "com.mysql.jdbc.Driver";
+	private static String URL = "jdbc:mysql://localhost/makeyourbet";
+	private static String USER = "ricardolino";
+	private static String PASSWD = "xxx";
+
+	public static Connection getConnection() throws SQLException {
+		try {
+			Class.forName(DRIVER);
+			return DriverManager.getConnection(URL, USER, PASSWD);
+		} catch (ClassNotFoundException e) {
+			throw new SQLException(e.getMessage());
+		}
+	}
 }
