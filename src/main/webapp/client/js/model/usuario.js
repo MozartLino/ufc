3468bde2ced560308cf1a4a125cb60d30ca70e94
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-06-05 11:25:12 -0300

    create update user

--- src/main/webapp/client/js/model/usuario.js
+++ src/main/webapp/client/js/model/usuario.js
@@ -1,15 +1,23 @@
 function usuarioForm($scope, $window, $http) {
 
 	$scope.salvaPaciente = function() {
-		var url = "/ufc/usuarios";
+		var url = "/#usuarios";
 
-		$http.post(url, {
-			"usuario" : $scope.usuario
+		$http({
+			method : getMethod($scope.usuario.id),
+			url : url,
+			data : {
+				"usuario" : $scope.usuario
+			}
 		}).success(function(data) {
 			$scope.usuarios = data;
-			$window.window.location.href = "http://localhost:8080/ufc/perfil";
+			$window.window.location.href = "/#perfil";
 		});
 	};
+}
+
+function getMethod(value) {
+	return value == null ? "post" : "put";
 }
 
 function usuarioRankingController($scope, $window, $http) {
