1d299dbcfa3d4e0ac568de895d428f7311a52e39
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-06-08 23:33:59 -0300

    alterando conexacao com o banco

--- src/main/java/br/com/each/ConnectionFactory/ConnectionFactory.java
+++ src/main/java/br/com/each/ConnectionFactory/ConnectionFactory.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
 	private static String DRIVER = "com.mysql.jdbc.Driver";
-	private static String URL = "jdbc:mysql://localhost/makeyourbet";
-	private static String USER = "root";
-	private static String PASSWD = "vimifero";
+	private static String URL = "jdbc:mysql://ec2-23-21-211-172.compute-1.amazonaws.com/makeyourbet";
+  	private static String USER = "ricardolino";
+  	private static String PASSWD = "xxx";
 
 	public static Connection getConnection() throws SQLException {
 		try {
