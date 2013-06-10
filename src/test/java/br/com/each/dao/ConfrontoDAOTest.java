fa606addf3b4c112aa6b2dd789c2d3141fbc1654
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-06-10 00:29:26 -0300

    removendo testes inválidos

--- src/test/java/br/com/each/dao/ConfrontoDAOTest.java
+++ src/test/java/br/com/each/dao/ConfrontoDAOTest.java
@@ -1,40 +1,6 @@
 package br.com.each.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import br.com.each.infra.PayloadUFCBD;
 
 public class ConfrontoDAOTest {
-
-	@Test
-	public void testaMetodoListaConfrontos() {
-		assertEquals("Nao trouxe todos os confrontos", 4, new ConfrontoDAO().lista().size());
-	}
-
-	@Test
-	public void testaMetodoListaConfrontosAbertos() {
-		assertEquals("Nao trouxe confrontos abertos", 1, new ConfrontoDAO().abertos().size());
-	}
-
-	@Test
-	public void testaMetodoListaConfrontosFechado() {
-		assertTrue("Nao ha confrontos Fechados, a lista deveria estar vazia", new ConfrontoDAO().fechados().isEmpty());
-	}
-
-	@Test
-	public void testaMetodoListaConfrontosFinalizados() {
-		assertEquals("Nao trouxe os confronto finalizados", 3, new ConfrontoDAO().finalizados().size());
-	}
-
-	@Before
-	public void setUp() {
-		PayloadUFCBD payloadUFCBD = new PayloadUFCBD();
-		payloadUFCBD.criaConexao();
-		payloadUFCBD.populaBanco();
-	}
 
 }
