443629e15a09232026423ab31c21ba4bd6ee0f0c
Ricardo Mozart lino <ricardomozartlino@Mac-mini-de-Ricardo.local>
2013-06-09 23:48:20 -0300

    create class evento

---
+++ src/main/java/br/com/each/model/confronto/Evento.java
@@ -0,0 +1,62 @@
+package br.com.each.model.confronto;
+
+public class Evento {
+
+	private Long id;
+
+	private String descricao;
+
+	private Status status;
+
+	public Evento(String descricao, Status status) {
+		this.descricao = descricao;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evento other = (Evento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", descricao=" + descricao + ", status=" + status + "]";
+	}
+
+}
