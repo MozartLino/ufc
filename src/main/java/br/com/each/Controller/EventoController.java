0afa1efc920d5e17cb2766f9c1c6777372c94dd2
Ricardo Mozart lino <ricardomozartlino@Mac-mini-de-Ricardo.local>
2013-06-09 23:49:31 -0300

    create controller evento

---
+++ src/main/java/br/com/each/Controller/EventoController.java
@@ -0,0 +1,36 @@
+package br.com.each.Controller;
+
+import br.com.caelum.vraptor.Consumes;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Put;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.each.dao.EventoDAO;
+import br.com.each.model.confronto.Evento;
+
+@Resource
+public class EventoController {
+
+	private Result result;
+	private EventoDAO eventoDAO;
+
+	public EventoController(Result result, EventoDAO eventoDAO) {
+		this.result = result;
+		this.eventoDAO = eventoDAO;
+	}
+
+	@Post("eventos")
+	@Consumes("application/json")
+	public void salva(Evento evento) {
+		eventoDAO.salva(evento);
+		result.nothing();
+	}
+
+	@Put("eventos")
+	@Consumes("application/json")
+	public void altera(Evento evento) {
+		eventoDAO.altera(evento);
+		result.nothing();
+	}
+
+}
